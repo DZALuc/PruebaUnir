8ae68b9820d288d5a53c63efb9103f4a708cdd5d
DZALuc <damianzu94@gmail.com>
2024-07-16 17:23:22 -0600

    Create Manejo_de_errores.py

---
+++ Manejo_de_errores.py
@@ -0,0 +1,31 @@
+# def suma():
+#     n1 = int(input("numero 1: "))
+#     n2 = int(input("numero 2: "))
+#     print(n1+n2)
+#     print("Gracias por sumar" + n1)
+
+# try:
+#     suma()
+# except TypeError:
+#     print("Estas concatenando tipos distintos") 
+# except ValueError:
+#     print("Eso no es un numero")     
+# else:
+#     print("Todo salio bien")
+# finally:
+#     print("Eso fue todo")       
+
+def pedir_numero():
+
+    while True:
+        try:
+            numero = int(input("Dame un numero: "))
+        except:
+            print("Ese no es un numero")    
+        else:
+            print(f"Ingresaste el numero {numero}") 
+            break
+
+    print("Grscias") 
+
+pedir_numero()
